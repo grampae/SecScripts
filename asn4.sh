270157ba143b4cb1cc11d6e3d7f04bf95d9bc79c
grampae <36344197+grampae@users.noreply.github.com>
2024-10-10 21:21:24 -0400

    Delete asn4.sh

--- asn4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- if [ "$#" -eq  "0" ]
-   then
-     echo "No arguments supplied"
-     echo "Usage: ./asn_v4.sh <ASN> "
-     exit
-else
-curl --silent https://www.dan.me.uk/bgplookup?asn=$1 |grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,2\}' |sort |uniq
-fi
